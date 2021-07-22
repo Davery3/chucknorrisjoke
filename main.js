401df38cc12d10cb2d50c10e1fc15a22c8ae8afb
Davery3 <80860555+Davery3@users.noreply.github.com>
2021-07-22 14:32:07 -0700

    edit

---
+++ main.js
@@ -0,0 +1,12 @@
+jokeButton=()=>{
+  let displayFact = document.querySelector(".display-joke");
+  let url = "https://api.chucknorris.io/jokes/random";
+  fetch(url)
+    .then(response=>{
+      return response.json()
+    })
+    .then(responseData=>{
+      console.log("response", responseData);
+      displayFact.innerHTML = responseData.value;
+    })
+}
